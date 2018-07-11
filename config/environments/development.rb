b404a61cdfc3298742cefa4a777ea3f091dd0d47
harry <ngocthaicao@gmail.com>
2018-07-12 00:53:33 +0700

    Add Register Confirmation Mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Action Mailer for Gmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      user_name:            Rails.application.credentials.dig(:gmail, :user_name),
+      password:             Rails.application.credentials.dig(:gmail, :password),
+      authentication:       'plain',
+      enable_starttls_auto: true }
+
+  routes.default_url_options = { host: 'localhost', port: 8080, protocol: 'http' }
 end
