fda36db2fca6f938692e606633fd2267290d1b6b
harry <ngocthaicao@gmail.com>
2018-08-08 20:00:01 +0700

    Remove questions json when not purchased

--- app/serializers/paper_serializer.rb
+++ app/serializers/paper_serializer.rb
@@ -3,21 +3,22 @@ class PaperSerializer < ActiveModel::Serializer
              :num_questions, :year, :round,
              :created_at, :updated_at
 
-  attribute :is_purchased
+  attribute :is_purchased?, if: :login?
 
-  has_many :questions
+  has_many :questions, if: :is_purchased?
 
-  def is_purchased
-    if @instance_options[:purchases].nil?
-      return false
-    end
-
+  def is_purchased?
+    !@instance_options[:purchases].nil? and
     @instance_options[:purchases].each do |purchase|
       if object.id == purchase.paper_id
         return true
       end
     end
 
-    return false
+    false
+  end
+
+  def login?
+    !@instance_options[:purchases].nil?
   end
 end
