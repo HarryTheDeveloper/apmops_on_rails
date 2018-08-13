d499563c1cd6454f48f59bb2f3f6af45b209cd4d
harry <ngocthaicao@gmail.com>
2018-08-13 11:43:59 +0700

    Move delete shop to purchase model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,8 +2,17 @@ class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :paper
 
+  before_create :delete_corresponding_shop
+
   validates_associated :user
   validates_associated :paper
 
   default_scope -> { order(updated_at: :desc) }
+
+  private
+
+  def delete_corresponding_shop
+    shop = Shop.find_by_user_id_and_paper_id(user_id, paper_id)
+    shop.destroy if shop
+  end
 end
